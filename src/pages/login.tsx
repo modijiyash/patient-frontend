cbce94e89076ce8f0c0f78238fd3e1cd13b3e499
modijiyash <yashpandeyboy@gmail.com>
2025-09-05 22:03:27 +0530

    fixed the login

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -15,7 +15,7 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const res = await fetch("https://patient-frontend-txxi.vercel.app//login", {
+      const res = await fetch("https://patient-frontend-txxi.vercel.app/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
